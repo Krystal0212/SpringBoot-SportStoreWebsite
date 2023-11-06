fc1b6a9da34e1362e36959034c95397efc7cd9d4
binhphap5 <tridungluong123@gmail.com>
2023-11-07 01:54:47 +0700

    import * trong login authen

--- src/main/resources/static/js/login-authentication.js
+++ src/main/resources/static/js/login-authentication.js
@@ -1,9 +1,9 @@
-import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
+import * as database from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
 
 // Function to find the customer with matching username and password
 const findMatchingCustomer = (snapshot, username, password) => {
-    for (const customerId in snapshot.val()) {
-        const customerData = snapshot.val()[customerId];
+    for (const snap in snapshot.val()) {
+        const customerData = snapshot.val()[snap];
         if (customerData.username === username && customerData.password === password) {
             console.log("Authentication successful!");
             // You can add code to redirect the user to a protected page here.
@@ -18,10 +18,10 @@ window.checkCredentials = function() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const dbRef = ref(getDatabase());
-    const customersRef = child(dbRef, "Customer");
+    const dbRef = database.getDatabase();
+    const customersRef = database.ref(dbRef, "Customer");
 
-    get(customersRef)
+    database.get(customersRef)
         .then((snapshot) => {
             if (snapshot.exists()) {
                 findMatchingCustomer(snapshot, username, password);
