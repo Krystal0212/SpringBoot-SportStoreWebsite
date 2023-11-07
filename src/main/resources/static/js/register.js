663c17135bf24c251bd4248e44da5d6061a1c295
binhphap5 <tridungluong123@gmail.com>
2023-11-08 01:08:27 +0700

    done register user

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,35 +1,53 @@
 import * as database from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
+
+const checkPasswordStrength = function checkPasswordStrength(password) {
+    // Kiểm tra mật khẩu có ít nhất 8 ký tự và chứa ít nhất 1 chữ in hoa
+    const passwordPattern = /^(?=.*[A-Z]).{8,}$/;
+    return passwordPattern.test(password);
+};
+
+const checkMatchingPasswords = function checkMatchingPasswords(password, passwordConfirm) {
+    return password === passwordConfirm;
+};
+
+const checkPhoneNumberLength = function checkPhoneNumberLength(phoneNumber) {
+    return phoneNumber.length >= 10 && phoneNumber.length < 13;
+};
+
+const checkUsernameLength = function checkUsernameLength(username) {
+    return username.length >= 5;
+};
+
 const checkEmailAndUsername = function checkEmailAndUsername(email, username) {
   const dbRef = database.getDatabase();
-  const usersRef = database.ref(dbRef, "Customer");
-
-  // Kiểm tra email đã tồn tại trong Firebase
-  return Promise.all([
-    database.get(database.child(usersRef, 'email/' + email)),
-    database.get(database.child(usersRef, 'username/' + username)),
-  ])
-    .then((snapshots) => {
-      const emailSnapshot = snapshots[0];
-      const usernameSnapshot = snapshots[1];
-
-      if (emailSnapshot.exists() && usernameSnapshot.exists()) {
-        // Cả email và username đã tồn tại
-        alert("Email and Username already exist in Firebase"); // Thay bằng thông báo hiển thị tùy theo giao diện của bạn
-        return false;
-      } else if (emailSnapshot.exists()) {
-        // Email đã tồn tại
-        alert("Email already exists in Firebase"); // Thay bằng thông báo hiển thị tùy theo giao diện của bạn
-        return false;
-      } else if (usernameSnapshot.exists()) {
-        // Username đã tồn tại
-        alert("Username already exists in Firebase"); // Thay bằng thông báo hiển thị tùy theo giao diện của bạn
-        return false;
-      } else {
-        // Cả email và username đều chưa tồn tại
-        return true;
-      }
-    });
-}
+  const customersRef = database.ref(dbRef, "Customer");
+    //One time read data user get
+    return database.get(customersRef)
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                for(const snap in snapshot.val()){
+                    const customerData = snapshot.val()[snap];
+                    if (customerData.username === username) {
+                        alert("Tên tài khoản đã được sử dụng");
+                        return false;
+                    }
+                    else if(customerData.email === email){
+                        alert("Email đã được sử dụng");
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                console.log("No data available in the 'Customer' node");
+                return false;
+            }
+        })
+        .catch((error) => {
+            console.error("Error code:", error.code);
+            console.error("Error message:", error.message);
+            return false;
+        });
+};
 
 const checkEmptyFields = function checkEmptyFields(username, password, email, fullName, gender, phoneNumber) {
   const emptyFields = [];
@@ -57,16 +75,16 @@ const checkEmptyFields = function checkEmptyFields(username, password, email, fu
     // Có ít nhất một trường thông tin bị bỏ trống
     const message = `Vui lòng điền đầy đủ thông tin: ${emptyFields.join(", ")}`;
     alert(message); // Thay bằng thông báo hiển thị tùy theo giao diện của bạn
-    return false;
+    return true;
   }
-  return true;
-}
+  return false;
+};
 const WriteDataToFirebase = function WriteDataToFirebase(username, password, email, fullName, gender, phoneNumber){
 
     const dbRef = database.getDatabase();
     const customersRef = database.ref(dbRef, "Customer");
 
-    database.get(customersRef)
+    return database.get(customersRef)
         .then((snapshot) => {
           // snapshot.val() chứa tất cả dữ liệu hiện có
           const userData = snapshot.val();
@@ -98,26 +116,62 @@ const WriteDataToFirebase = function WriteDataToFirebase(username, password, ema
           };
 
           // Ghi dữ liệu người dùng vào cơ sở dữ liệu Firebase với ID mới
-          return database.set(database.ref(dbRef, 'Customer/' + newID), newUser);
+            database.set(database.ref(dbRef, 'Customer/' + newID), newUser);
+          return true;
         })
         .catch((error) => {
           console.error("Error code:", error.code);
           console.error("Error message:", error.message);
+          return false;
         });
 };
 
- window.ClickRegister = function ClickRegister(){
-    //thêm hàm để check có thông tin nào bị bỏ trống
-    // hàm check xem email và username có tồn tại trong firebase chưa
-    console.log("checkCredentials function called");
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const email = document.getElementById('email').value;
-    const fullName = document.getElementById('name').value;
-    const gender = document.getElementById('gender').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    if(checkEmptyFields(username,password, email, fullName, gender, phoneNumber)){
-        WriteDataToFirebase(username,password, email, fullName, gender, phoneNumber);
-        window.location.href = "/login";
-    }
-};
+ window.ClickRegister = function ClickRegister() {
+     //thêm hàm để check có thông tin nào bị bỏ trống
+     // hàm check xem email và username có tồn tại trong firebase chưa
+     console.log("checkCredentials function called");
+     const username = document.getElementById('username').value;
+     const password = document.getElementById('password').value;
+     const passwordConfirm = document.getElementById('passwordConfirm').value;
+     const email = document.getElementById('email').value;
+     const fullName = document.getElementById('name').value;
+     const gender = document.getElementById('gender').value;
+     const phoneNumber = document.getElementById('phoneNumber').value;
+     if (checkEmptyFields(username, password, email, fullName, gender, phoneNumber)) {
+         return;
+     }
+
+     if (!checkUsernameLength(username)) {
+         alert("Tên tài khoản phải có ít nhất 5 kí tự.");
+         return;
+     }
+
+     if (!checkPasswordStrength(password)) {
+         alert("Mật khẩu phải có ít nhất 8 ký tự và chứa ít nhất 1 chữ in hoa.");
+         return;
+     }
+
+     if (!checkMatchingPasswords(password, passwordConfirm)) {
+         alert("Mật khẩu và xác nhận mật khẩu phải giống nhau.");
+         return;
+     }
+
+     if (!checkPhoneNumberLength(phoneNumber)) {
+         alert("Số điện thoại phải có ít nhất 10 số và bé hơn 13 số.");
+         return;
+     }
+
+     checkEmailAndUsername(email, username)
+         .then((result) => {
+             if (result) {
+                 WriteDataToFirebase(username, password, email, fullName, gender, phoneNumber)
+                     .then(() => {
+                         alert("Tạo tài khoản thành công, vui lòng đăng nhập để tiếp tục !");
+                         window.location.href = "/login";
+                     })
+                     .catch((error) => {
+                         console.error("Error writing user data: ", error);
+                     });
+             }
+         });
+ };
