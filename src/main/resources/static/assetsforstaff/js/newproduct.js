be3463f7d2a62254350bc43b426f445620e06cde
emtucobac <outlast0102@gmail.com>
2023-11-11 23:52:21 +0700

    staff update: +product img +add product

--- src/main/resources/static/assetsforstaff/js/newproduct.js
+++ src/main/resources/static/assetsforstaff/js/newproduct.js
@@ -2,50 +2,48 @@ import * as database from "https://www.gstatic.com/firebasejs/10.5.2/firebase-da
 // Get the Firebase database
 // Add an event listener to the submit button
 
-const WriteDataToFirebase = function WriteDataToFirebase(username, password, email, fullName, gender, phoneNumber){
+const WriteDataToFirebase = function WriteDataToFirebase(pName, pBrand, pType, pPrice, pQuantity){
 
     const dbRef = database.getDatabase();
     const pRef = database.ref(dbRef, "Product");
-
     return database.get(pRef)
         .then((snapshot) => {
-          // snapshot.val() chứa tất cả dữ liệu hiện có
-          const pData = snapshot.val();
+        if(pType === "Shoes")
+        {
+                   const pRefShoes = database.ref(pRef, "Shoes");
+                   return database.get(pRefShoes)
+                   .then((snapshot) => {
 
-          // Tìm người dùng mới có ID theo mẫu
-          const latestID = Object.keys(pData).reduce((maxID, key) => {
-            const match = key.match(/^S(\d+)$/);
-            if (match) {
-              const currentID = parseInt(match[1]);
-              if (currentID > maxID) {
-                maxID = currentID;
-              }
-            }
-            return maxID;
-          }, 0);
+                   const pData = snapshot.val();
 
-          // Tạo ID mới
-          const newID = `S${(latestID + 1).toString().padStart(2, "0")}`;
+                                     const latestID = Object.keys(pData).reduce((maxID, key) => {
+                                       const match = key.match(/^S(\d+)$/);
+                                       if (match) {
+                                         const currentID = parseInt(match[1]);
+                                         if (currentID > maxID) {
+                                           maxID = currentID;
+                                         }
+                                       }
+                                       return maxID;
+                                     }, 0);
 
-          // Tạo dữ liệu người dùng
-          const newUser = {
-            pName: String(pName),
-            pBrand: String(pBrand),
-            pPrice: String(pPrice),
-            pQuantity: String(pQuantity),
-            state: 'enable',
-          };
+                                     // Tạo ID mới
+                                     const newID = `S${(latestID + 1).toString().padStart(2, "0")}`;
 
-          // Ghi dữ liệu người dùng vào cơ sở dữ liệu Firebase với ID mới
-          if(pType === "Shoes")
-          {
-          database.set(database.ref(dbRef, 'Product/' + 'Shoes/' + newID), newProduct);
-          }
-          if(pType === "Shirt")
-                    {
-                    database.set(database.ref(dbRef, 'Product/' + 'Shirt/' + newID), newProduct);
-                    }
-          return true;
+                                     // Tạo dữ liệu người dùng
+                                     const newProduct = {
+                                       pName: String(pName),
+                                       pBrand: String(pBrand),
+                                       pPrice: String(pPrice),
+                                       pQuantity: String(pQuantity),
+                                       state: 'enable',
+                                     };
+                                     // Ghi dữ liệu người dùng vào cơ sở dữ liệu Firebase với ID mới
+                                     database.set(database.ref(dbRef, 'Product/' + 'Shoes/' + newID), newProduct);
+                                     return true;
+                   })
+
+        }
         })
 };
 window.addPButton = function addPButton() {
